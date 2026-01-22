03aa72e2174630a7529a3b22900ca87dee04c79b
kyryl-bogach <kyryl.bogach@gmail.com>
2026-01-22 20:20:07 +0100

    Draft: Enable auto theme switching

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" data-theme="light">
+    <html lang="en" data-theme="auto">
       <head>
         <link
           rel="stylesheet"
